95b63468af8b6c562195ecba21ed1f74a931de6b
Johannes Staffans <johannes.staffans@artory.com>
2017-11-01 23:05:52 +0100

    Style update

--- content/posts/2015-01-11-a-new-blog-is-born.md
+++ content/posts/2015-01-11-a-new-blog-is-born.md
@@ -1,5 +1,4 @@
 ---
-layout: post
 title:  "A new blog is born"
 date:   2015-01-11
 tags:   [blog, about]
--- src/site/common.clj
+++ src/site/common.clj
@@ -19,7 +19,16 @@
    [:div.header__content.clearfix
     [:div.sm-col.sm-col-9
      [:div "personal website and blog of"]
-     [:h1.header__headline.pb2 "Johannes Staffans"]]
+     [:h1.header__headline "Johannes Staffans"]]
     [:div.sm-col
-     [:a {:href "#"} "about"]]]]
-  )
+     [:a {:href "#"} "about"]]]
+   ;; <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="svg-triangle">
+   ;;   <polygon points="0,0 100,0 50,100"/>
+   ;; </svg>
+   [:div.clearfix
+    [:svg.left.border-triangle {:xmlns "http://www.w3.org/2000/svg" :version "1.1"}
+     [:polygon.mask {:points "0,0 30,0 30,30 0,30"}]
+     [:polygon {:points "0,15 30,0 30,30"}]]
+    [:svg.right.border-triangle {:xmlns "http://www.w3.org/2000/svg" :version "1.1"}
+     [:polygon.mask {:points "0,0 30,0 30,30 0,30"}]
+     [:polygon {:points "30,15 0,30 0,0"}]]]])
